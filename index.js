0fd874a4ff50ca4718d477101033c93bc59e63c7
truongnhatanh7 <72392353+truongnhatanh7@users.noreply.github.com>
2022-02-25 15:03:05 +0700

    Add game logic

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
+'use strict'
+
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
 const cells = $$(".board-game__cell")
+const currentRemainingFlag = $(".board-info__remaining-bombs")
+const winMsg = $(".board__win-info")
+const loseMsg = $(".board__lose-info")
+
 
 let board = [...Array(18).keys()].map(i => Array(18).fill(0))
-// for (let i = 0; i < 18; i++) {
-//     for (let j = 0; j < 18; j++) {
-//         console.log(board[i][j])
-//     }
-// }
+let isOpen = [...Array(18).keys()].map(i => Array(18).fill(false))
+let prevColor = [...Array(18).keys()].map(i => Array(18).fill(""))
+let randomValues = null
+let endGame = false;
+
 main()
 
 function main() {
     cellColoring()
-    cellListener()
     generateBombs()
     calculateBoard()
 
-    console.log(board)
-    
 }
 
 function cellColoring() {
     let tempFlag = 0
-    let row = 0
     let col = 0
     let count = 0
-    for (cell of cells) {
+
+    for (let cell of cells) {
         cell.setAttribute("id","cell_" + count)
+        cell.addEventListener("click", cellListener)
+        cell.addEventListener("contextmenu", function(e) {
+            e.preventDefault()
+            if (cell.classList.contains("enable-flag")) {
+                cell.classList.remove("enable-flag")
+                currentRemainingFlag.innerHTML = parseInt(currentRemainingFlag.innerHTML) + 1
+            } else {
+                cell.classList.add("enable-flag")
+                currentRemainingFlag.innerHTML = parseInt(currentRemainingFlag.innerHTML) - 1
+                
+            }
+            
+        })
         if (tempFlag === 0) {
             cell.style.backgroundColor = "#32a852"
             tempFlag++
@@ -50,7 +66,19 @@ function cellColoring() {
     }
 }
 
-function random(max = 20, col = 18 * 18) {
+function checkWinningCondition() {
+
+}
+
+function normalCoordtoYX(coord) {
+
+}
+
+function YXToNormalCoord(y, x) {
+    
+}
+
+function random(max = 30, col = 18 * 18) {
     const set = new Set()
     while (set.size < max) {
         let rand = Math.floor(Math.random() * Math.floor(col))
@@ -60,13 +88,13 @@ function random(max = 20, col = 18 * 18) {
 }
 
 function generateBombs() {
-    // Generate 10 random bombs
-    let randomValues = random()
+    // Generate 30 random bombs
+    randomValues = random()
 
-    for (value of randomValues) {
-        let x = Math.floor(value / 18)
-        let y = Math.floor(value % 18) - 1
-        board[x][y] = -1
+    for (let value of randomValues) {
+        let y = Math.floor(value / 18)
+        let x = Math.floor(value % 18)
+        board[y][x] = -1
     }
 
 }
@@ -96,10 +124,54 @@ function calculateBoard() {
     }
 }
 
+function cellFlag() {
+    // let stringId = this.getAttribute("id")
+    // let numId = parseInt(stringId.substring(5))
+    // let cellCoor = {y: Math.floor(numId / 18), x: Math.floor(numId % 18)}
+    // if (this.style.backgroundColor === "red") {
+    //     this.style.backgroundColor = "#ccc"
+    // } else {
+        this.style.backgroundColor = "red"
+    // }
+}
+
 function cellListener() {
-    for (cell of cells) {
-        cell.onclick = () => {
-            cell.style.backgroundColor = "red"
-        }
+    let stringId = this.getAttribute("id")
+    let numId = parseInt(stringId.substring(5))
+    let cellCoor = {y: Math.floor(numId / 18), x: Math.floor(numId % 18)}
+    console.dir(cellCoor)
+    this.innerHTML = `<p>${board[cellCoor.y][cellCoor.x]}</p>`
+    this.style.backgroundColor = "#ccc"
+    if (board[cellCoor.y][cellCoor.x] === -1) {
+        console.log("fuck")
+        loseMsg.classList.add("pop-msg")
     }
+    exploreSurroundings(cellCoor.y, cellCoor.x)
+}
+
+function exploreSurroundings(y, x) {
+    if (y < 0 || x < 0 || y == 18 || x == 18 || board[y][x] === -1 || isOpen[y][x]) {
+        return
+    }
+
+    let tempCell = $("#cell_" + (18 * y + x))
+    console.log(tempCell.getAttribute("id"))
+    isOpen[y][x] = true
+    tempCell.innerHTML = `<p>${board[y][x]}</p>`
+    tempCell.style.backgroundColor = "#ccc"
+    
+    if (board[y][x] !== 0) { // > 0 Values
+        return
+    } 
+
+    // DFS
+    exploreSurroundings(y - 1, x - 1)
+    exploreSurroundings(y, x - 1)
+    exploreSurroundings(y + 1, x - 1)
+    exploreSurroundings(y - 1, x)
+    exploreSurroundings(y + 1, x)
+    exploreSurroundings(y - 1, x + 1)
+    exploreSurroundings(y, x + 1)
+    exploreSurroundings(y + 1, x + 1)
+
 }
